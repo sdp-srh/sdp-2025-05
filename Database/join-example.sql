ef7073bac6957f663c230472c2bf31afb248d5e6
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2025-05-20 15:09:41 +0200

    join example

--- Database/join-example.sql
+++ Database/join-example.sql
@@ -1,7 +1,10 @@
 
 
-SELECT b.book_id, b.title, b.publication_year, a.author_id, a.last_name, a.first_name
-	FROM book as b
-	INNER JOIN book_author as ba on b.book_id = ba.book_id
-	INNER JOIN author as a on ba.author_id = a.author_id
-	ORDER BY publication_year
+SELECT b.book_id, b.title,a. last_name, a.first_name, b.publication_year, b.pages, p.name
+	FROM book as b 
+	INNER JOIN publisher as p ON b.publisher_id = p.publisher_id
+	INNER JOIN book_author as ba ON b.book_id = ba.book_id 
+	INNER JOIN author as a ON ba.author_id = a.author_id 
+	where p.name LIKE 'Penguin%' and a.last_name = 'King' and b.pages > 600
+	ORDER BY b.publication_year 
+	LIMIT 2
